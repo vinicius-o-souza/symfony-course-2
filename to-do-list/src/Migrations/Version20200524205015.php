d74d2f5836821230aee8ccf57f0eda898bbe81b3
Vinícius de Oliveira Souza <vinicius.souza@justdigital.com.br>
2020-05-31 10:38:42 -0300

    Adicao do docker no projeto

--- to-do-list/src/Migrations/Version20200524205015.php
+++ to-do-list/src/Migrations/Version20200524205015.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200321143303 extends AbstractMigration
+final class Version20200524205015 extends AbstractMigration
 {
     public function getDescription() : string
     {
